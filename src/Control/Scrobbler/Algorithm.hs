57ffef3d66b7704ac28696b104b256bc9974a6dd
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-06-12 14:00:47 +0400

    Allow users to supply their own scrobbling rules if they want.

--- src/Control/Scrobbler/Algorithm.hs
+++ src/Control/Scrobbler/Algorithm.hs
@@ -1,5 +1,12 @@
--- | Common scrobbling algorithm routines
-module Control.Scrobbler.Algorithm (contest, time') where
+-- | Scrobbling algorithm routines
+module Control.Scrobbler.Algorithm
+  ( -- * Contesters
+    contest, contestWith
+    -- * Scrobbling rules
+  , Rules
+    -- * Auxilliary
+  , time'
+  ) where
 
 import Control.Monad (liftM)
 import Data.Int (Int64)
@@ -14,32 +21,57 @@ import Data.Time.Clock.POSIX (getPOSIXTime)
 import Control.Scrobbler.Types
 
 
--- | Check if candidate is ready to be scrobbled
+-- | Scrobbling rules
 --
--- How to scrobble nicely: <http://www.lastfm.ru/api/scrobbling>
-contest :: MonadIO m => Scrobbler m (PlayerStateChange Track) (Scrobble (Timed Track))
-contest = contest' . (time' &&& id)
+-- First argument is time elapsed since scrobbler started
+--
+-- Second argument is contested candidate track
+--
+-- Result is either some scrobbler failure (most probably 'NoScrobble') or scrobbled track
+--
+-- Note: users should update '_local' themselves
+type Rules = Int64 -> Timed Track -> Either ScrobblerError (Scrobble (Timed Track))
 
-contest' :: MonadIO m => Scrobbler m (Int64, PlayerStateChange Track) (Scrobble (Timed Track))
-contest' = mkStateM Stopped $ \_dt ((t, ch), tr) -> do
+
+-- | Check if candidate is ready to be scrobbled with default 'rules'
+contest :: MonadIO m
+        => Scrobbler m (PlayerStateChange Track) (Scrobble (Timed Track))
+contest = contestWith rules
+
+-- | Check if candidate is ready to be scrobbled with custom rules
+contestWith :: MonadIO m
+            => Rules
+            -> Scrobbler m (PlayerStateChange Track) (Scrobble (Timed Track))
+contestWith f = contestWith' f . (time' &&& id)
+
+contestWith' :: MonadIO m
+             => Rules
+             -> Scrobbler m (Int64, PlayerStateChange Track) (Scrobble (Timed Track))
+contestWith' f = mkStateM Stopped $ \_dt ((t, ch), tr) -> do
   lt <- round `liftM` liftIO getPOSIXTime
-  return (change (Left NoScrobbles) (go t) tr, ch <&> \tr' -> def
+  return (change (Left NoScrobbles) (f t) tr, ch <&> \tr' -> def
     & datum .~ tr'
     & start .~ t
     & local .~ lt)
+
+
+-- | Default rules for scrobbling
+--
+-- Reference: <http://www.lastfm.ru/api/scrobbling>
+rules :: Rules
+rules t tr
+  -- If candidate length is less than 30 seconds, we do not scrobble it
+  | tr^.datum.length < 30 = Left NoScrobbles
+  -- Otherwise, if the time passed since is more than half candidate lenght it's
+  -- definitely should be scrobbled
+  | dt > tr^.datum.length `div` 2 = Right . Scrobble $ tr & local +~ dt
+  -- Otherwise, if the passed is more than 4 minutes it's should be scrobbled anyway
+  | dt > 4 * 60 = Right . Scrobble $ tr & local +~ dt
+  -- Otherwise there is nothing to scrobble
+  | otherwise = Left NoScrobbles
  where
-  go t tr
-    -- If candidate length is less than 30 seconds, we do not scrobble it
-    | tr^.datum.length < 30 = Left NoScrobbles
-    -- Otherwise, if the time passed since is more than half candidate lenght it's
-    -- definitely should be scrobbled
-    | dt > tr^.datum.length `div` 2 = Right . Scrobble $ tr & local +~ dt
-    -- Otherwise, if the passed is more than 4 minutes it's should be scrobbled anyway
-    | dt > 4 * 60 = Right . Scrobble $ tr & local +~ dt
-    -- Otherwise there is nothing to scrobble
-    | otherwise = Left NoScrobbles
-   where
-    dt = t - tr^.start
+  dt = t - tr^.start
+
 
 change :: b -> (a -> b) -> PlayerStateChange a -> b
 change _ f (Started a) = f a
