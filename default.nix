515f7949a76cb12a446e35eae61bad3e1b206b5b
Matvey Aksenov <matvey.aksenov@gmail.com>
2014-11-05 19:22:30 +0000

    Cosmetics

--- default.nix
+++ default.nix
@@ -1,18 +1,17 @@
-{ cabal, aeson, cereal, exceptions, hspec, httpClient
-, httpTypes, lens, lensAeson, liblastfm, libmpd, mtl, netwire, network
+{ cabal, aeson, cereal, exceptions, hspec, httpClient, httpTypes
+, lens, lensAeson, liblastfm, libmpd, mtl, netwire, network
 , QuickCheck, semigroups, text, time
 }:
 
-cabal.mkDerivation (self: rec {
+cabal.mkDerivation (self: {
   pname = "scrobblers";
   version = "0.1.0.0";
   src = ./.;
   buildDepends = [
-    aeson cereal exceptions httpClient httpTypes
-	lens lensAeson liblastfm libmpd mtl netwire network semigroups
-	text time
+    aeson cereal exceptions httpClient httpTypes lens lensAeson
+    liblastfm libmpd mtl netwire network semigroups text time
   ];
-  testDepends = buildDepends ++ [ hspec QuickCheck ];
+  testDepends = [ hspec lens netwire network QuickCheck ];
   meta = {
     description = "Lastfm scrobblers";
     license = self.stdenv.lib.licenses.bsd3;
