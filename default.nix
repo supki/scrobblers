edc56f9a0c36a28a8edf6348bd614fb213e9a07b
Dmitry Malikov <malikov.d.y@gmail.com>
2015-06-11 18:11:03 +0000

    Modernize nix expressions, test only ghc-7.10.1 in travis

--- default.nix
+++ default.nix
@@ -1,19 +1,2 @@
-{ haskellPackages ? (import <nixpkgs> {}).haskellPackages }:
-
-haskellPackages.cabal.mkDerivation (self: {
-  pname = "scrobblers";
-  version = "0.1.0.0";
-  src = ./.;
-  buildDepends = with haskellPackages; [
-    aeson cereal exceptions httpClient httpTypes lens lensAeson
-    liblastfm libmpd mtl netwire network semigroups text time
-  ];
-  testDepends = with haskellPackages; [
-    hspec lens netwire network QuickCheck
-  ];
-  meta = {
-    description = "Lastfm scrobblers";
-    license = self.stdenv.lib.licenses.bsd3;
-    platforms = self.ghc.meta.platforms;
-  };
-})
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
+nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./scrobblers.nix { }
